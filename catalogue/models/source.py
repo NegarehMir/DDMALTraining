fc79051bbccbf788b633ed170212501b05add492
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2016-05-03 16:55:44 -0400

    New: source fields are nullable
    the following model fields are now null by default
    Source.start_date, Source.end_date, Source.archive

--- catalogue/models/source.py
+++ catalogue/models/source.py
@@ -7,9 +7,12 @@ class Source(models.Model):
 
     shelfMark = models.CharField(max_length=255)
     name = models.CharField(max_length=255, blank=True, null=True)
-    start_date = models.IntegerField()
-    end_date = models.IntegerField()
+    start_date = models.IntegerField(blank=True, null=True)
+    end_date = models.IntegerField(blank=True, null=True)
     type = models.CharField(max_length=127)
     surface = models.CharField(max_length=127)
     comments = models.TextField(blank=True, null=True)
-    archive = models.ForeignKey("catalogue.Archive")
+    archive = models.ForeignKey("catalogue.Archive", blank=True, null=True)
+
+    def __str__(self):
+        return "{0}".format(self.name)
