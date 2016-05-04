31875a46ec6fe1a96f5f32293e40b117f9a40e4b
Nicky <negareh.mirfallahliarestani@mail.mcgill.ca>
2016-05-04 10:48:06 -0400

    New: import_csv now supports all models

--- catalogue/management/commands/import_csv.py
+++ catalogue/management/commands/import_csv.py
@@ -2,6 +2,9 @@ import csv
 from django.core.management.base import BaseCommand
 from catalogue.models.source import Source
 from catalogue.models.archive import Archive
+from catalogue.models.composer import Composer
+from catalogue.models.composition import Composition
+from catalogue.models.composed import Composed
 
 
 class Command(BaseCommand):
@@ -42,12 +45,43 @@ class Command(BaseCommand):
         return archive
 
 
+    def _get_composition(self, row):
+        composition, created = Composition.objects.get_or_create(title=row['composition_name']);
+
+        if created:
+            if row['composition_name']:
+                composition.title = row['composition_name']
+            composition.save()
+
+        return composition
+
+
+    def _get_composer(self, row):
+        if row['composer'].startswith('?'):
+            composer, created = Composer.objects.get_or_create(name=row['composer'][1:]);
+        else:
+            composer, created = Composer.objects.get_or_create(name=row['composer']);
+
+        if created:
+            if row['composer']:
+                composer.name = row['composer']
+            composer.save()
+
+        return composer
+
+
 
     def handle(self, *args, **options):
         print('Deleting sources')
         Source.objects.all().delete()
         print('Deleting archive')
         Archive.objects.all().delete()
+        print('Deleting composer')
+        Composer.objects.all().delete()
+        print('Deleting composition')
+        Composition.objects.all().delete()
+        print('Deleting composed')
+        Composed.objects.all().delete()
 
         with open('diamm_excerpt.csv', 'r') as csvfile:
             contents = csv.DictReader(csvfile)
@@ -57,3 +91,40 @@ class Command(BaseCommand):
                 print("Importing row {0}".format(rownum))
                 source = self._get_source(row)
                 archive = self._get_archive(row)
+                composition = self._get_composition(row)
+                composer = self._get_composer(row)
+                composed = Composed()
+
+                if not source.archive:
+                    source.archive = archive
+                    source.save()
+
+                if not composition.source:
+                    composition.source = source
+                    composition.save()
+
+                if not composition.composer:
+                    if composer.name == "no composer/anon":
+                        composition.anonymous = True
+                    composition.composer = composer
+                    composition.save()
+
+                if composer.name.startswith('?'):
+                    composed.certain = False
+                    composer.name = composer.name[1:]
+                    composer.save()
+                if not composed.composition:
+                    composed.composition = composition
+                if not composed.composer:
+                    composed.composer = composer
+                composed.save()
+
+
+
+
+
+
+
+
+
+
