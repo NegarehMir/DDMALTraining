8a999111d9abd70ac94ae3f62a3d56c63dcf699f
Nicky <negareh.mirfallahliarestani@mail.mcgill.ca>
2016-05-05 15:12:00 -0400

    New: archive view completed

--- catalogue/views/archive.py
+++ catalogue/views/archive.py
@@ -1,13 +1,17 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from catalogue.models.archive import Archive
-
+from catalogue.serializers.website.archive import ArchiveSerializer
 
 class ArchiveListView(ListAPIView):
     #Handels GET "/archives/"
-    pass
+    template_name = "archive/archive_list.html"
+    queryset = Archive.objects.all()
+    serializer_class = ArchiveSerializer
 
 
 
 class ArchiveDetailView(RetrieveAPIView):
     # HAndles GET "/archives/1/"
-    pass
+    template_name = "archive/archive_detail.html"
+    queryset = Archive.objects.all()
+    serializer_class = ArchiveSerializer
