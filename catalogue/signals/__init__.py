1c34cd58249ca0ce3a50a3eee20383c2394d28a7
Nicky <negareh.mirfallahliarestani@mail.mcgill.ca>
2016-05-05 10:24:29 -0400

    New: signal files generated

---
+++ catalogue/signals/__init__.py
@@ -0,0 +1,5 @@
+import catalogue.signals.source_signals
+import catalogue.signals.archive_signals
+import catalogue.signals.composition_signals
+import catalogue.signals.composer_signals
+import catalogue.signals.composed_signals
